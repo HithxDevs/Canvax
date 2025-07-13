c87c1afdfdc2d61807ed335b768a152931c46885
HARI SUPRIYA DARABOINA <daraboinaharisupriya@gmail.com>
2025-07-13 10:04:01 +0530

    pushing n-2

--- apps/http-be/src/index.ts
+++ apps/http-be/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '@repo/be-common/config';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,7 +9,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/signup', (req, res) => {
+  // Here you would handle the signup logic
+  res.send('Signup endpoint');
+});
 
+app.post('/signin', (req, res) => {
+  // Here you would handle the signin logic
+  res.send('Signin endpoint');
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
